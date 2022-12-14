88b2d056b9845307a7fd19f1b247c4f93c0ab32b
Girish Muley <girish.muley007@gmail.com>
2022-12-14 19:58:10 +0530

    types of for loops in js

---
+++ loops.js
@@ -0,0 +1,39 @@
+//A loop is a sequence of instructions that is continually repeated until a certail condition is reached
+//following are the loops in js
+//For loop
+//For in loop
+//For of loop
+//while loop
+//do-while loop
+
+//For loop
+//loop a block of code no of times
+let sum = 0;
+let n = 4;
+for (let i = 0; i < n; i++) {
+  sum += i + 1;
+}
+console.log("sum of first " + n + " natural numbers is " + sum);
+
+//For in loop
+//loop through the keys of an object
+
+let obj = {
+  Girish: 90,
+  Shailesh: 95,
+  Onkar: 80,
+  Vishal: 60,
+};
+
+for (let name in obj) {
+  console.log("Mark of " + name + " is " + obj[name]);
+}
+
+//for of loop
+//loop through the values of an object
+
+for (let value of "Girish") {
+  console.log(value);
+}
+
+//
