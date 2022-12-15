0481ebbbf3eb4cf4119344d0fa98566ef5919a0b
Girish Muley <girish.muley007@gmail.com>
2022-12-15 20:54:17 +0530

    function in js

---
+++ function.js
@@ -0,0 +1,10 @@
+//function is a set of statements that performs a task or calculates a value, but for a procedure to qualify as a function, it should take some input and return an output where there is some obvious relationship between the input and the output
+//write a function to calculate a average of two number
+
+function avgOftwo(p, q) {
+  return (p + q) / 2;
+}
+let a = 2;
+let b = 2;
+
+console.log("Average of two number is " + avgOftwo(a, b));
