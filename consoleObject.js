44a872dc63a1c0e0e9f4c393d3596c270cdba5d2
Girish Muley <girish.muley007@gmail.com>
2022-12-26 22:29:00 +0530

    console object

---
+++ consoleObject.js
@@ -0,0 +1,21 @@
+//consle object methods
+//the console object has several mehods, log beging one of them. some of them areas follows
+
+//used to assert a condition
+console.assert(2 > 5);
+
+//output a message to console using log
+console.log("output a message to console using log");
+
+//display a table data
+obj = { a: 1, b: 2 };
+console.table(obj);
+
+//used for warning
+console.warn("warning");
+
+//use for error
+console.error("error");
+
+//used for special info
+console.info("info");
