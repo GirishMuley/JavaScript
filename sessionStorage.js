8b2b79c232228d8c0ac1c49b0a979b119973a7fa
Girish Muley <girish.muley007@gmail.com>
2023-02-03 20:16:02 +0530

    sessionStorage & related methods

---
+++ sessionStorage.js
@@ -0,0 +1,10 @@
+// sessionStorage.setItem()
+// sessionStorage.getItem()
+// sessionStorage.removeItem()
+// sessionStorage.key()
+// sessionStorage.clear()
+
+window.onstorage = (e) => {
+  alert("changed");
+  console.log(e);
+};
