e81a594da3997ee6b6809c3b9e62cb65a2ea0c1f
Girish Muley <girish.muley007@gmail.com>
2023-02-04 23:24:44 +0530

    Inheritance and extends in js

---
+++ inheritanceAndExtends.js
@@ -0,0 +1,24 @@
+class Animal {
+  constructor(name, color) {
+    this.name = name;
+  }
+  run() {
+    console.log(this.name + " is running");
+  }
+  shout() {
+    console.log(this.name + " is Shouting");
+  }
+}
+
+class Monkey extends Animal {
+  eatBanana() {
+    console.log(this.name + " is eating banana");
+  }
+}
+
+let ani = new Animal("Bruno", "white");
+let m = new Monkey("Chimpu", "orange");
+
+ani.shout();
+m.eatBanana();
+m.shout();
