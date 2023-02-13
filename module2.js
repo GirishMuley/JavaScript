dbab96494b7afb0b7c30723d2031e0fbab6e2cb8
Girish Muley <girish.muley007@gmail.com>
2023-02-13 21:04:51 +0530

    ES6 module in js

---
+++ module2.js
@@ -0,0 +1,11 @@
+export const hello = () => {
+  console.log("hello harry");
+};
+
+export const ahello = (name) => {
+  console.log("hello " + name);
+};
+
+export default harry = () => {
+  console.log("hello" + "Harry");
+};
--- modules.js
+++ modules.js
@@ -1,3 +1,5 @@
-const { hello, ahello } = require("./module1");
+// const { hello, ahello } = require("./module1");//commen module
+import harry, { hello, ahello } from "./module2.js"; //es6 module
 hello();
 ahello("shivani");
+harry();
